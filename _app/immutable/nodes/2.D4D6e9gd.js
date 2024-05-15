f70d27813e4d4819a46f398431f882fbb73dfbe8
Ricard Penin Honrubia <ergoegos@gmail.com>
2024-05-15 21:42:11 +0200

    Updates

--- _app/immutable/nodes/2.D4D6e9gd.js
+++
@@ -1,1 +0,0 @@
-import{s}from"../chunks/scheduler.BvmtPxVN.js";import{S as t,i as e}from"../chunks/index.D6Kqn3tr.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
