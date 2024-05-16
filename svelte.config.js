789307fc1e06cdcf17a0b08daa39baf2d0deb287
Ricard Penin Honrubia <ergoegos@gmail.com>
2024-05-16 02:17:54 +0200

    testing to automate deploy workflow

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,32 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+
+
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
+    }
+  }
 };
 
 export default config;
+// export default {
+//   kit: {
+// 		paths: {
+// 			base: "/josepe"
+// 		},
+//     adapter: adapter({
+//       // default options are shown. On some platforms
+//       // these options are set automatically — see below
+//       pages: 'dist',
+//       assets: 'dist',
+//       fallback: null,
+//       precompress: false,
+//       domain: '',
+//       jekyll: false
+//     })
+//   }
+// };
